4941df57c3ed9188ba2891b6f40297158d64f934
Sergio Rojas <sergio.rojas.aragon09@gmail.com>
2019-07-14 20:27:31 -0400

    Fin Seccion 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
+
+// routemodule hace que angular tome las rutas principales o rutas hijas.
 import { Routes, RouterModule } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
 
-const routes: Routes = [];
+const app_routes : Routes = [
+    { path: 'home', component: PortafolioComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'item', component: ItemComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'home' }
 
+];
+
+// en los imports se colocan los modulos
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(app_routes, { useHash: true})
+    ],
+    exports: [
+        RouterModule
+    ]
 })
-export class AppRoutingModule { }
+
+// se exporta porque se debe utilizar en otros lados.
+export class AppRoutingModule{
+
+}
